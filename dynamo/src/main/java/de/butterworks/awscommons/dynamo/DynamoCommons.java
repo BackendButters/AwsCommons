7f5e4f80e515c32b4891b344ed49731b1b202aec
Nico <jdam@live.de>
2017-04-29 22:24:35 +0200

    Introduce local DynamoDB mode

--- dynamo/src/main/java/de/butterworks/awscommons/dynamo/DynamoCommons.java
+++ dynamo/src/main/java/de/butterworks/awscommons/dynamo/DynamoCommons.java
@@ -1,13 +1,14 @@
 package de.butterworks.awscommons.dynamo;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.model.DescribeTableRequest;
 import com.amazonaws.services.dynamodbv2.model.ResourceNotFoundException;
-import com.amazonaws.services.dynamodbv2.model.TableDescription;
 import com.amazonaws.services.dynamodbv2.model.TableStatus;
+import com.amazonaws.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,25 +16,35 @@ public final class DynamoCommons {
 
     private static final Logger logger = LoggerFactory.getLogger(DynamoCommons.class);
 
-    private final AmazonDynamoDBClient client;
     private final DynamoDB db;
 
     private static DynamoCommons instance = null;
 
-    private final static String awsAccessKey = System.getenv("awsAccessKey") == null ? System.getProperty("awsAccessKey") : System.getenv("awsAccessKey");
-    private final static String awsSecretKey = System.getenv("awsSecretKey") == null ? System.getProperty("awsSecretKey") : System.getenv("awsSecretKey");
-    private final static Regions region = System.getenv("awsRegion") == null ? Regions.fromName(System.getProperty("awsRegion").toLowerCase()) : Regions.fromName(System.getenv("awsRegion").toLowerCase());
-
     private DynamoCommons() {
-        logger.info("Using Region " + region.name());
-        logger.info("Using Access Key " + awsAccessKey);
 
-        client = new AmazonDynamoDBClient(new BasicAWSCredentials(awsAccessKey, awsSecretKey))
-                .withRegion(region);
+        if (StringUtils.isNullOrEmpty(System.getenv("awsAccessKey")) && StringUtils.isNullOrEmpty(System.getProperty("awsAccessKey"))) {
+            // local mode
+            logger.warn("AWS access key was null or blank. Falling back to localhost:8000");
 
-        db = new DynamoDB(new AmazonDynamoDBClient(
-                new BasicAWSCredentials(awsAccessKey, awsSecretKey))
-                .withRegion(region));
+            db = new DynamoDB(AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(
+                    new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-west-2"))
+                    .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("obacht", "dummy")))
+                    .build());
+
+        } else {
+            final String awsAccessKey = System.getenv("awsAccessKey") == null ? System.getProperty("awsAccessKey") : System.getenv("awsAccessKey");
+            final String awsSecretKey = System.getenv("awsSecretKey") == null ? System.getProperty("awsSecretKey") : System.getenv("awsSecretKey");
+            final Regions region = System.getenv("awsRegion") == null ? Regions.fromName(System.getProperty("awsRegion").toLowerCase()) : Regions.fromName(System.getenv("awsRegion").toLowerCase());
+
+            logger.info("Using Region " + region.name());
+            logger.info("Using Access Key " + awsAccessKey);
+
+            db = new DynamoDB(AmazonDynamoDBClientBuilder
+                    .standard()
+                    .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsAccessKey, awsSecretKey)))
+                    .withRegion(region)
+                    .build());
+        }
     }
 
     DynamoDB getDb() {
@@ -47,9 +58,7 @@ public final class DynamoCommons {
 
     boolean tableExists(final String tableName) {
         try {
-            final TableDescription table = client.describeTable(new DescribeTableRequest(tableName))
-                    .getTable();
-            return TableStatus.ACTIVE.toString().equals(table.getTableStatus().toUpperCase());
+            return TableStatus.ACTIVE.toString().equals(db.getTable(tableName).describe().getTableStatus().toUpperCase());
         } catch (final ResourceNotFoundException rnfe) {
             return false;
         }
