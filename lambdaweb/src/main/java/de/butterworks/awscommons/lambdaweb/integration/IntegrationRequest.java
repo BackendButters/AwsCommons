38af8152c5046dedf739ebd35bd3ddec5b69f5bb
Nico Scherer <jdam@live.de>
2017-05-21 21:22:33 +0200

    Add uid & groups support to integration request

--- lambdaweb/src/main/java/de/butterworks/awscommons/lambdaweb/integration/IntegrationRequest.java
+++ lambdaweb/src/main/java/de/butterworks/awscommons/lambdaweb/integration/IntegrationRequest.java
@@ -1,14 +1,25 @@
 package de.butterworks.awscommons.lambdaweb.integration;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 public class IntegrationRequest {
 
     private String action;
 
     private IntegrationRequestBody body;
 
-    public IntegrationRequest(final String action, final IntegrationRequestBody body) {
+    private String uid;
+
+    private String groups;
+
+    public IntegrationRequest(final String action, final IntegrationRequestBody body, final String uid, final String groups) {
         this.action = action;
         this.body = body;
+        this.uid = uid;
+        this.groups = groups;
     }
 
     public IntegrationRequest() {
@@ -28,5 +39,29 @@ public class IntegrationRequest {
 
     public void setBody(IntegrationRequestBody body) {
         this.body = body;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(final String uid) {
+        this.uid = uid;
+    }
+
+    public String getGroups() {
+        return groups;
+    }
+
+    public void setGroups(final String groups) {
+        this.groups = groups;
+    }
+
+    public UUID getParsedUid() {
+        return UUID.fromString(uid);
+    }
+
+    public List<String> getParsedGroups() {
+        return  Arrays.stream(groups.split(",")).collect(Collectors.toList());
     }
 }
